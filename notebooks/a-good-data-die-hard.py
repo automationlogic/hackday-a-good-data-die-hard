f03ede5c26d6e793af56661b996f4ac300a3275c
JakeFisher77 <66358460+JakeFisher77@users.noreply.github.com>
2020-06-03 16:42:03 +0100

    Final working solution

--- notebooks/a-good-data-die-hard.py
+++ notebooks/a-good-data-die-hard.py
@@ -1,15 +1,10 @@
 # Databricks notebook source
-display(dbutils.fs.ls("/databricks-datasets/samples/docs/"))
-display(dbutils.fs.ls("/FileStore/tables/"))
-
-# COMMAND ----------
-
 # MAGIC %python
 # MAGIC import urllib.request
 # MAGIC 
-# MAGIC # (csv_filename, headers) = urllib.request.urlretrieve("https://cve.mitre.org/data/downloads/allitems.csv")
-# MAGIC # print(csv_filename)
-# MAGIC # dbutils.fs.cp("file:/databricks/driver%s" % csv_filename, "dbfs:/datasets/allitems.csv")
+# MAGIC (csv_filename, headers) = urllib.request.urlretrieve("https://cve.mitre.org/data/downloads/allitems.csv","/databricks/driver/allitems.csv")
+# MAGIC dbutils.fs.mv("file:%s" % csv_filename, "dbfs:/Filestore/tables/allitems.csv")
+# MAGIC 
 # MAGIC data = spark.read.csv("dbfs:/FileStore/tables/allitems.csv", header="true", inferSchema="true")
 # MAGIC data.cache()
 # MAGIC data = data.dropna()
@@ -18,24 +13,10 @@ display(dbutils.fs.ls("/FileStore/tables/"))
 
 # COMMAND ----------
 
-from pyspark.sql.functions import regexp_extract, col
-
-result = data.withColumn('OS', regexp_extract(col('Description'), '.*(Android|Windows|BSD|Linux|MacOS).*', 1))
+from pyspark.sql.functions import regexp_extract, regexp_replace, col, lower
+year_column = data.withColumn('Year', regexp_extract(col('Name'), '^CVE-([0-9]*)-.*', 1))
+result = year_column.withColumn('OperatingSystem', regexp_extract(col('Description'), '.*(?i)(Android|Windows|BSD|Linux|macOS|OSX|OS X|iOS).*', 1)).withColumn('OperatingSystem', lower(col('OperatingSystem'))).withColumn('OperatingSystem', regexp_replace('OperatingSystem', 'macos|osx|os x', 'macos'))
 result.createOrReplaceTempView("os_column")
-
-# COMMAND ----------
-
-# MAGIC %sql
-# MAGIC SELECT * FROM os_column WHERE LENGTH(OS) > 1;
-
-# COMMAND ----------
-
-dbutils.fs.mount(
-  source = "wasbs://databricks-output@datahardstorage.blob.core.windows.net/",
-  mount_point = "/mnt/databricks-output",
-  extra_configs = {"fs.azure.account.key.datahardstorage.blob.core.windows.net":dbutils.secrets.get(scope = "cve", key = "datahardstorage-access-key")})
-
-result.write.format("parquet").save("/mnt/databricks-output/mitre-cve.parquet")
 
 # COMMAND ----------
 
@@ -44,12 +25,17 @@ configs = {"fs.adl.oauth2.access.token.provider.type": "ClientCredential",
            "fs.adl.oauth2.credential": dbutils.secrets.get(scope = "cve", key = "sp-key"),
            "fs.adl.oauth2.refresh.url": "https://login.microsoftonline.com/6b4c2372-ae97-4555-af13-7fc2b57dfe28/oauth2/token"}
 
-# Optionally, you can add <directory-name> to the source URI of your mount point.
-# dbutils.fs.mount(
-#   source = "adl://adlaagddh.azuredatalakestore.net/databricks-output",
-#   mount_point = "/mnt/databricks-output-data-lake-gen1",
-#   extra_configs = configs)
+# Check if the ADL store is mounted already, and if not, mount it
+if not any(mount.mountPoint == '/mnt/databricks-output-data-lake-gen1' for mount in dbutils.fs.mounts()):
+  dbutils.fs.mount(
+    source = "adl://adlaagddh.azuredatalakestore.net/",
+    mount_point = "/mnt/databricks-output-data-lake-gen1",
+    extra_configs = configs
+  )
 
+result.write.format("parquet").mode("overwrite").save("/mnt/databricks-output-data-lake-gen1/mitre-cve.parquet")
 
-dbutils.fs.ls("/mnt/databricks-output-data-lake-gen1/")
-result.write.format("parquet").save("/mnt/databricks-output-data-lake-gen1/mitre-cve.parquet")
+# COMMAND ----------
+
+# MAGIC %sql
+# MAGIC SELECT * FROM os_column WHERE NULLIF(OperatingSystem, '') IS NOT NULL LIMIT 1000;
